ec87ab00fed42369758b28f78a8bd53496ca4164
Senuka Chandunu <senukaacc@gmail.com>
2025-02-20 23:50:05 +0530

    Lab Sheet 1 : SUM

---
+++ Lab/Lab Sheet 1/3/3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ using System.Threading.Tasks;
+ 
+ namespace MyNamespace
+ {
+         class Program
+         {
+             static void Main(string[] args)
+             {
+                 Console.Write("Enter a Positive integer: ");
+                 int num = int.Parse(Console.ReadLine());
+                 
+                 if(num < 0)
+                 {
+                     Console.WriteLine("Error: Please enter a positive integer");
+                     return;
+                 }
+ 
+                 int result = sum(num);
+                 Console.WriteLine(result);
+                 
+                 static int sum(int num)
+                 {
+                     int sum = 0;
+                     for (int i = 1; i <= num; i++)
+                     {
+                         sum += i;
+                     }
+                     return sum;
+                 }
+             }
+         }
+ }
