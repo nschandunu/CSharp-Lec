3e0fe1b3e5b2a949c6670f763ba9ca3ea1a9d87c
Senuka Chandunu <senukaacc@gmail.com>
2025-02-20 22:32:06 +0530

    Lab Sheet 1 : AREA

---
+++ Lab/Lab Sheet 1/1/1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Enter length: ");
+                int length = int.Parse(Console.ReadLine());
+                Console.Write("Enter width: ");
+                int width = int.Parse(Console.ReadLine());
+                
+                area(length, width);
+                Console.WriteLine("Area of the rectangle is " + area(length, width));
+                static int area(int length, int width)
+                {
+                    return length * width;
+                }
+            }
+        }
+}
