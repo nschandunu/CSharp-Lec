7e0aaa36b30af0230b63987e3fe17c2dd6723f23
Senuka Chandunu <senukaacc@gmail.com>
2025-02-20 23:33:17 +0530

    Lab Sheet 1 : ODD - EVEN

---
+++ Lab/Lab Sheet 1/2/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter a number between 1 - 10");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num / 2 == 0)
+                {
+                    Console.WriteLine("The number is even");
+                }
+                else
+                {
+                    Console.WriteLine("The number is odd");
+                }
+            }
+        }
+}
