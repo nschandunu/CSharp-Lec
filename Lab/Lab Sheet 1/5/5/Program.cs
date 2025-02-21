e8ceb6da75eec23b6a003e4e1363378c867dba3c
Senuka Chandunu <senukaacc@gmail.com>
2025-02-21 19:30:55 +0530

    Lab Sheet 1 : Multi. Table

---
+++ Lab/Lab Sheet 1/5/5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nMultiplication Table of " + number + ":");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} x {i} = {number * i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
