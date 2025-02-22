78d73328b0247c877ed25e8f09a1b896abf1ce2e
Senuka Chandunu <senukaacc@gmail.com>
2025-02-22 22:21:21 +0530

    Lab Sheet 1 : Grading System

--- Lab/Lab Sheet 1/6/6/Program.cs
+++ Lab/Lab Sheet 1/6/6/Program.cs
@@ -10,7 +10,32 @@ namespace MyNamespace
         {
             static void Main(string[] args)
             {
-                
+                Console.WriteLine("Enter Your Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Exam Mark: ");
+                int mark = int.Parse(Console.ReadLine());
+                string grade = "";
+                if (mark >= 75)
+                {
+                    grade = "A";
+                }
+                else if (mark >= 60)
+                {
+                    grade = "B";
+                }
+                else if (mark >= 50)
+                {
+                    grade = "C";
+                }
+                else if (mark >= 40)
+                {
+                    grade = "D";
+                }
+                else
+                {
+                    grade = "Failed";
+                }
+                Console.WriteLine($"Hey, {name}, Your Grade is {grade}.");
             }
         }
 }
--- Lab/Lab Sheet 1/6/6/obj/Debug/net9.0/6.AssemblyInfo.cs
+++ Lab/Lab Sheet 1/6/6/obj/Debug/net9.0/6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8ceb6da75eec23b6a003e4e1363378c867dba3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963f753b2536a9b1e1c4d84e660bcc9c9e7137b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
